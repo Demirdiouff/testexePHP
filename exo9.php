0dd337d0e2fdc015e853b41d910f748593cf38f1
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-09-29 23:56:13 +0200

    tentatives exo

--- exo9.php
+++ exo9.php
@@ -9,18 +9,41 @@ echo '<p><strong>Ex : 9</strong></p>';
 // Mais si l'heure est 23 h 59 min 59 s alors -> 00 h 00 min 00 s
 
 
-$heureSaisi = null;
-$minuteSaisi = null;
-$secondeSaisi = null;
+$heure = null;
+$minutes = null;
+$secondes = null;
 
-if (isset($_POST['heureSaisi'], $_POST['minuteSaisi'], $_POST['secondeSaisi'])) {
-    $heureSaisi = $_POST['heureSaisi'];
-    $minuteSaisi = $_POST['minuteSaisi'];
-    $secondeSaisi = $_POST['secondeSaisi'];
+$nouvHeure = null;
+$nouvMinutes = null;
+$nouvSecondes = null; 
+
+
+if (isset($_POST['heure'], $_POST['minutes'], $_POST['secondes'])) {
+    $heure = $_POST['heure'];
+    $minutes = $_POST['minutes'];
+    $secondes = $_POST['secondes'];
 }
 
 if (isset($_POST['ajouterUneSeconde'])){
-    
+    if (isset($_POST['secondes'], $_POST['minutes'], $_POST['heure'])) {
+        $secondes = $nouvSecondes;
+        $nouvSecondes = $_POST['secondes'];
+        $minutes = $nouvMinutes;
+        $nouvMinutes = $_POST['minutes'];
+        $heure = $nouvHeure;
+        $nouvHeure = $_POST['heure'];
+        $nouvSecondes++;
+    } if ($nouvSecondes > 59) {
+        $nouvSecondes = 00;
+        $nouvMinutes++;
+        if ($nouvMinutes > 59) {
+            $nouvMinutes = 00;
+            $nouvHeure++;
+            if ($nouvHeure > 23) {
+                $nouvHeure = 00;
+            }
+        }
+    }
 }
 
 
@@ -34,21 +57,23 @@ if (isset($_POST['ajouterUneSeconde'])){
 	<hr style="width: 150px; float: left">
 
 	<form method="POST"><br>
-		<p><input style="width: 200px" type="number" min="00" max="23" name="heureSaisi" placeholder="Heure :  (entre 00 et 23)" value="<?php echo $heureSaisi?>"></p>
-		<p><input style="width: 200px" type="number" min="00" max="59" name="minuteSaisi" placeholder="Minutes :  (entre 00 et 59)" value="<?php echo $minuteSaisi?>"></p>
-		<p><input style="width: 200px" type="number" min="00" max="59" name="secondeSaisi" placeholder="Secondes :  (entre 00 et 59)" value="<?php echo $secondeSaisi?>"></p>
+		<p><input style="width: 200px" type="number" min="00" max="23" name="heure" placeholder="Heure :  (entre 00 et 23)" value="<?php echo $heure?>"></p>
+		<p><input style="width: 200px" type="number" min="00" max="59" name="minutes" placeholder="Minutes :  (entre 00 et 59)" value="<?php echo $minutes?>"></p>
+		<p><input style="width: 200px" type="number" min="00" max="59" name="secondes" placeholder="Secondes :  (entre 00 et 59)" value="<?php echo $secondes?>"></p>
 		<input type="submit" name="enregistrementHeure" value="Rentrez cette heure">
 	</form><br>
 	
-	<?php if (isset($_POST['enregistrementHeure'])){?>
-	<p>Voici l'heure que vous avez rentré : <?php echo $heureSaisi . 'h '; echo $minuteSaisi . 'min '; echo $secondeSaisi . 's.' ?></p>
+	<?php if (isset($_POST['enregistrementHeure'])){ ?>
+	<p>Voici l'heure que vous avez rentré : <?php echo $heure . 'h '; echo $minutes . 'min '; echo $secondes . 's.' ?></p>
+	<?php } ?>
 	
+	<?php if (isset($_POST['enregistrementHeure'])) {?>
 	<form method="POST">
-		<p><input style="width: 200px" type="hidden" min="00" max="23" name="heureSaisi" placeholder="Heure :  (entre 00 et 23)" value="<?php echo $heureSaisi?>"></p>
-		<p><input style="width: 200px" type="hidden" min="00" max="59" name="minuteSaisi" placeholder="Minutes :  (entre 00 et 59)" value="<?php echo $minuteSaisi?>"></p>
-		<p><input style="width: 200px" type="hidden" min="00" max="59" name="secondeSaisi" placeholder="Secondes :  (entre 00 et 59)" value="<?php echo $secondeSaisi?>"></p>
+		<p><input style="width: 200px" type="hidden" min="00" max="23" name="heure" placeholder="Heure :  (entre 00 et 23)" value="<?php echo $nouvHeure?>"></p>
+		<p><input style="width: 200px" type="hidden" min="00" max="59" name="minutes" placeholder="Minutes :  (entre 00 et 59)" value="<?php echo $nouvMinutes?>"></p>
+		<p><input style="width: 200px" type="hidden" min="00" max="59" name="secondes" placeholder="Secondes :  (entre 00 et 59)" value="<?php echo $nouvSecondes?>"></p>
 		<input type="submit" name="ajouterUneSeconde" value="Ajouter une seconde ?">
 	</form>
-	<p>Voici la nouvelle heure actuelle, on avance dans le temps... : <?php ?></p>	
-	<?php }?>
+	<p>Voici la nouvelle heure actuelle, on avance dans le temps... : <?php echo $nouvHeure . 'h '; echo $nouvMinutes . 'min '; echo $nouvSecondes . 's.' ?></p>
+	<?php } ?>	
 </body>
