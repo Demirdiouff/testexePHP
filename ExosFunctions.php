851cc613bd4447c7869439fb0ec9bdf215303c8b
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-11-03 22:38:15 +0100

    ex sur le principe de functions PHP

--- ExosFunctions.php
+++ ExosFunctions.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'Debug.php';
+
 echo '<a href="index.php">Retour au menu</a><br><br><br>';
 
 // Exercices via le site
@@ -53,6 +55,8 @@ function areaBis($l, $w){
     return $areaBis;
 }
 
+$area = areaBis(2, 3);
+echo $area;
 echo "<p>A rectangle of lenght 2 and width 3 has an area of " . areaBis(2, 3) . ".</p>";
 
 
@@ -214,6 +218,191 @@ if (!isset($_POST['submit'])){
 
 <?php 
 // ------------------------------------------------------------------------ 5) Variable Argument Number, GO ! ------------------------------------------------------------------- //
+// Dans cet exercice on va reprendre les éléments qu'on avait stocké dans une checkbox, a savoir la température dans l'exo 3 des array
+// On va stocker ces éléments la dans une fonction pour que ce soit plus pratique d'utilisation.
+// il faut retourner les paramètres rentrés dans la checkbox en majuscule pour la première lettre (ucwords permet de le faire)
 
+// il y a une deuxième partie pour cet exercice, car il faut pouvoir ajouter une météo si jamais ceux existant ne sont pas suffisant 
+// donc on va créer une deuxième fonction pour pouvoir réaliser cela
+
+// On définit la fonction qui va nous servir a stocker les éléments : 
+function checkBox(){
+    $arguments = func_get_args(); // Cette fonction ici nous permet de ne pas préciser de nombre d'arguments a l'avance 
+    // maintenant on effectue la boucle pour récupérer les élements : 
+    foreach ($arguments as $a) {
+        echo "<input type=\"checkbox\" name=\"weather[]\" value=\"$a\">" . ucwords($a) . "<br>";
+    }
+    //return $arguments;
+}
+
+// On définit la deuxième fonction
+function listerMet(){
+    $listArguments = func_get_args();
+    $arguments = $listArguments[0];
+//    Debug::printr($arguments);
+    Debug::printr($listArguments);
+    // ici on aura une petite particularité, il faudra tester savoir si ce qui a été saisi est sous forme array ou non, si 
+    // ça ne l'est pas, il faut le transformer en array
+    if (!is_array($arguments)){
+        $arguments = explode(',', $arguments);
+    }
+//     echo "============";
+//     Debug::printr($arguments[0][0]);
+//     echo "============";
+    // on effectue une boucle a travers le tableau 
+    foreach ($arguments as $a){
+        echo "<li>" . ucwords($a) . "</li>";
+    }
+}
+
+?>
+
+<h2>How's your weather now?</h2>
+
+<?php 
+// si le formulaire n'est pas encore soumis : 
+if (!isset($_POST['submit'])) {
+?>
+<form method="post">
+	<p>Veuillez entrer vos réponses :</p>
+	City : <input type="text" name="city"><br>
+	Month : <input type="text" name="month"><br>
+	Year : <input type="text" name="year">
+	<p>Veuillez choisir le temps qu'il fait :<br>(Choisissez tous ceux appliquable)</p>
+	<?php // ici on utilise la fonction qu'on a crée plus haut 
+        checkBox('sunshine', 'clouds', 'rain', 'hail', 'sleet', 'snow', 'wind', 'cold', 'heat');
+    ?>
+    <p />
+    
+	<p>Quelque chose a rajouter concernant les météo ? Veuillez les ajouter en les séparant par des virgule.</p>
+	<input type="text" name="plusMet" size="60" /><p />
+	<input type="submit" name="submit" value="Enregistrer" />
+</form>
+<?php 
+// si le formulaire a bien été envoyé alors on procède au script suivant : 
+} else {
+    // On recupère les informations sur la ville et le mois et l'année et on les stocke dans une variable qui sera un tableau array : 
+    $donneesUtilisateur = array(
+        $_POST['city'], 
+        $_POST['month'],
+        $_POST['year'],
+    );
+
+echo "A $donneesUtilisateur[0] durant le mois de $donneesUtilisateur[1] $donneesUtilisateur[2] vous avez enregistré une température plutôt : <br /><ul>";
+
+$meteo = $_POST['weather'];
+$plusMet = $_POST['plusMet'];
+
+// on appelle la fonction pour chaque méthode : 
+listerMet($meteo);
+listerMet($plusMet);
+
+echo "<ul>";
+
+}
+// ici il faut bien vérifier qu'on a utilisé la fonction pour mettre en majuscule la première lettre ds la fonction
+// et on se contente d'afficher ce qui a été coché et on le stocke dans weather comme crée dans la fonction
+
+// foreach ($meteo as $m) {
+//     echo "<li>" . ucwords($m) . "</li>\n";
+// }
+// echo "</ul>";
+// }
+
+Debug::printr($_POST);
+?>
+
+
+
+
+
+
+
+<br><br><br><hr><br><br>
+
+
+
+
+
+
+<?php
+// ------------------------------------------------------------------------ 6) Function That Calls Another, GO ! ------------------------------------------------------------------- //
+// Dans cet exercice on reprend le tableau de l'ex 3 des fonctions, avec le nb de jours dans le mois
+// On va transformer la fonction "options" en "makeOptions" pour qu'il prenne en compte comment créer une option 
+// sans qu'on le précise avant
+
+$monthsEx6 = array(
+        'January'=>31,
+        'February'=>'28 days, if leap year 29',
+        'March'=>31,
+        'April'=>30,
+        'May'=>31,
+        'June'=>30,
+        'July'=>31,
+        'August'=>31,
+        'September'=>30,
+        'October'=>31,
+        'November'=>30,
+        'December'=>31
+    );
+
+// Cette fonction permet de créer les options 
+function makeOptions($value){
+    foreach ($value as $k => $v){
+        // l'option a besoin des valeurs de $k 
+        echo "<option value=\"$k\">" . ucfirst($k) . "</option><br>";
+    }
+}
+
+// Cette fonction va faire appel a makeOptions pour créer les champs select
+function makeSelect($name, $value){
+    if(!is_array($value)){
+        exit("Erreur : value n'est pas un array.");
+        // ca aurait aussi pu être écrit die("Erreur : value n'est pas un array");
+    }
+    // on commence le HTML avec le select field 
+    echo "<select name=\"$name\">\n";
+    // on crée les options maintenant 
+    makeOptions($value);
+    echo "</select>";
+}
+
+?>
+
+<h2>Days in Month Again</h2>
+
+<?php 
+
+if (!isset($_POST['submit'])) {
+?>
+
+<form method="post">
+	<p>Veuillez choisir un mois :</p>
+
+<p />
+
+<?php 
+
+makeSelect('monthEx6', $monthsEx6);
+
+?>
+
+<p />
+<input type="submit" value="Choisir">
+</form>
+
+<?php
+// si le formulaire a été soumis alors on procède au formulaire
+} else {
+    // rien ne change ici par rapport a l'exo précédent 
+    $monthEx6 = $_POST['monthEx6'];
+    if ($monthEx6 == 'February'){
+        echo " Le mois de Fevrier a" . $monthsEx6['February'] . " . ";
+    } else {
+        echo "Le mois de $monthEx6 a $monthsEx6[$monthEx6] jours.";
+    }
+}
+
+Debug::printr($_POST);
 
 ?>
